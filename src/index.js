d7691b21eb69d27ac66518eddf7ba4faeb9a8820
Ero-124 <khachatryanero.2002@mail.ru>
2023-02-23 15:14:40 +0400

    update path

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./containers/App/App";
 import ThemeProvider from "./context/ThemeProvider";
+import { REPO_NAME } from "./repo";
 import store from "./store/store";
 import "./styles/index.css";
 
@@ -11,11 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
+          <App /> 
+        </ThemeProvider>{" "}
+      </BrowserRouter>{" "}
+    </Provider>{" "}
   </React.StrictMode>
 );
